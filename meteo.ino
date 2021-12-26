c423e114c4a73294404ff47164a10b787af7a008
batnoe <96292219+batnoe@users.noreply.github.com>
2021-12-26 09:49:57 +0100

    nettoyage code

--- meteo.ino
+++ meteo.ino
@@ -29,7 +29,6 @@ typedef struct struct_message {
     float c;
     //float d;
 } struct_message;
-
 struct_message myData;
 
 BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
@@ -136,9 +135,6 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {    
   temp_ext = myData.c;
    if (temp_ext > t_max) {t_max = temp_ext;} else if(temp_ext < t_min and t_min > -30 and temp_ext > -50) {t_min = temp_ext;}    // -------- calcul mini et maxi température extérieur ---------------
   myGLCD.setTextColor(TFT_RED,TFT_BLACK);
-  //if (temp_ext < 10)  { myGLCD.drawFloat(temp_ext, 1, 220, 90, 6); } else { myGLCD.drawFloat(temp_ext, 1, 210, 90, 6); }  // affiche température extérieur
-  //myGLCD.setTextColor(TFT_WHITE,TFT_BLACK); myGLCD.drawFloat(t_max, 1, 150, 80, 5); myGLCD.drawFloat(t_min, 1, 150, 110, 5);  //affiche mini maxi
-
   myGLCD.setTextColor(TFT_ORANGE,TFT_BLACK); myGLCD.drawFloat(temp_ext, 1, 130, 330, 8);
   myGLCD.setTextColor(TFT_RED,TFT_BLACK); myGLCD.drawFloat(t_max, 1, 10, 320, 6); myGLCD.setTextColor(TFT_BLUE,TFT_BLACK); myGLCD.drawFloat(t_min, 1, 10, 390, 6);  //affiche mini maxi
 
